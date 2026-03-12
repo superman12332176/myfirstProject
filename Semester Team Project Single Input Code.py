d06450a301dd3a3cd14c05ee11d3a600f94ab547
NetworkMGM <marcusmylesbiz@gmail.com>
2026-03-11 18:14:56 -0600

    Semester Team Project Single Input Code
    IMPORTANT: Due March 15 at 11:59 PM
    
    SEMESTER TEAM PROJECT
    SINGLE INPUT CODE
    This is a GROUP submission. Only ONE teammate needs to submit this code.
    Within your project code (.py file) create the initial input following:
    1. Give the user information on what input is needed
    2. Use well named variables
    3. Prompt the user to enter the needed information
    4. Print and describe the information (user feedback)
    5. Comment as appropriate
    6. Be creative
    At the top of your file, you need to establish a project preamble containing:
    # Team Name:
    # Teammates:
    # Course: PPSC CSC 1019
    # Project Topic and Description:
    # Change Log:
    # DATE Programmer Description
    # ----------------------------------------------------------
    # 13 Apr 26 Charlie Added list statistics
    #
    You will continue to build on this program, adding functionality as we learn python. Take
    time to think of what inputs might be needed. This file contains one input step.
    At this point, you will be using single variables. Later, you might switch to list or other
    options.

---
+++ Semester Team Project Single Input Code.py
@@ -0,0 +1,39 @@
+# Team Name: The Pioneers
+# Teammates: Marcus, Oakley, Ruben, Nathan
+# Course: PPSC CSC 1019
+# Project Topic and Description:Project Criteria
+#   1. Give the user information on what input is needed
+#   2. Use well named variables
+#   3. Prompt the user to enter the needed information
+#   4. Print and describe the information (user feedback)
+#   5. Comment as appropriate
+#   6. Be creative
+# Change Log:
+# DATE Programmer Description
+# ----------------------------------------------------------
+# 11 Mar 2026 Marcus Created the initial text file for the Semester Team Project Single Input Code
+# I dont think that the ammount of comments that im using is necessary but I think more context is always better than not enough.
+
+#1: Give the user information on what input is needed.
+#   So basically any input prompt must print "Please provide your...(insert desired info)" and cant just be unprompted
+
+#2. Use well named variables
+#   Self explainitory but I personaly like to use camelCase
+
+#3. Prompt the user to enter the needed information
+#   required to use the input function (i guess)
+
+#4. Print and describe the information (user feedback)
+#   Add extra context upon the output for example instead of a program printing "22" it should print "The box has 22 items inside"
+
+#5. Comment as appropriate
+#   self explainitory (oops lol)
+
+#6. Be creative
+#   XD
+
+#11 mar 2026 Marcus end comment
+
+##################################################
+
+#Insert Python code here and remember to document what you do
